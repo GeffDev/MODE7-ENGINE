38b0bb9a6a967a244ef6532080dbcb647cdea6d4
GeffDev <88239718+GeffDev@users.noreply.github.com>
2022-08-09 20:33:56 +0800

    literally todoing rn

--- src/core/log.zig
+++ src/core/log.zig
@@ -5,5 +5,7 @@ const LogWarnEnabled: bool = true;
 const LogInfoEnabled: bool = true;
 const LogDebugEnabled: bool = true;
 
+// add a build type check to disable select log type
+
 const LogTypes = enum { LogError = 0, LogFatal = 1, LogWarn = 2, LogInfo = 3, LogDebug = 4 };
 
