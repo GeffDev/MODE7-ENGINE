08eeeeb1ee06b4375ad2aef20046be5ff6a2e4e0
GeffDev <88239718+GeffDev@users.noreply.github.com>
2022-08-10 20:32:09 +0800

    who up logging they engine

--- src/core/log.zig
+++ src/core/log.zig
@@ -1,14 +1,32 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
+const LogTypes = enum(u8) { LogError = 0, LogFatal = 1, LogWarn = 2, LogInfo = 3, LogDebug = 4 };
+
 var LogWarnEnabled: bool = true;
 var LogInfoEnabled: bool = true;
 var LogDebugEnabled: bool = true;
 
-const LogTypes = enum(u8) { LogError = 0, LogFatal = 1, LogWarn = 2, LogInfo = 3, LogDebug = 4 };
-
-pub export fn BuildTypeLogCheck() void {
+pub export fn buildTypeLogCheck() void {
     if (builtin.Mode == .ReleaseSmall || .ReleaseFast) {
         LogDebugEnabled = false;
     }
 }
+
+pub export fn initLog() bool {
+    return true;
+}
+
+pub export fn cleanUpLog() void {}
+
+pub export fn logInput(log_type: LogTypes, message: []const u8) void {
+    if (log_type == LogTypes.LogDebug) {
+        if (LogDebugEnabled == false) {
+            return;
+        }
+    }
+
+    logOutput(log_type, message);
+}
+
+pub export fn logOutput(log_type: LogTypes, message: []const u8) void {}
--- src/core/platform.zig
+++ src/core/platform.zig
@@ -1,7 +1,7 @@
 const target = @import("builtin").target;
 const std = @import("std");
 
-pub export fn CheckPlatform() [*:0]const u8 {
+pub export fn checkPlatform() [*:0]const u8 {
     switch (target.os.tag) {
         .linux => {
             return "LINUX";
