c48d45048a033dc6599080d9a9d18db4b1824713
GeffDev <88239718+GeffDev@users.noreply.github.com>
2022-08-13 07:57:00 +0800

    ehlp

--- src/core/log.zig
+++ src/core/log.zig
@@ -9,7 +9,7 @@ var LogDebugEnabled: bool = true;
 
 pub export fn buildTypeLogCheck() void {
     comptime {
-        if (builtin.mode == .ReleaseSmall or .ReleaseFast) {
+        if (builtin.Mode == @Type(.ReleaseSmall) or @Type(.ReleaseFast)) {
             LogDebugEnabled = false;
         }
     }
