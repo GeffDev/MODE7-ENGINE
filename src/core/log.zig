817dfd5d11ef2c5d2df75721959b66224056ddd8
GeffDev <88239718+GeffDev@users.noreply.github.com>
2022-08-15 17:18:26 +0800

    adding more log_type switches

--- src/core/log.zig
+++ src/core/log.zig
@@ -33,6 +33,15 @@ pub fn writeToLog(log_type: LogTypes, message: [*:0]const u8) !void {
         LogTypes.LogDebug => {
             try stdout.print("[DEBUG]: {s}\n", .{message});
         },
+        LogTypes.LogFatal => {
+            try stdout.print("[FATAL]: {s}\n", .{message});
+        },
+        LogTypes.LogInfo => {
+            try stdout.print("[INFO]: {s}\n", .{message});
+        },
+        LogTypes.LogWarn => {
+            try stdout.print("[WARNING]: {s}\n", .{message});
+        },
         else => return,
     }
 }
